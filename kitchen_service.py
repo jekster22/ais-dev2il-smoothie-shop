514103d92d254351681d964e70bd42426b68a378
Javid <javid22h@gmail.com>
2026-04-24 09:10:42 +0200

    Kitchen logging

--- kitchen_service.py
+++ kitchen_service.py
@@ -1,12 +1,25 @@
 import asyncio
 import random
-import logging
-logger = logging.getLogger(__name__)
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import logging
+from prometheus_fastapi_instrumentator import Instrumentator
+# Custom metric: Count smoothies ordered by flavor
+from prometheus_client import Counter
 
+
+smoothies_ordered = Counter(
+    'smoothies_ordered_total',
+    'Total number of smoothies ordered',
+    ['flavor']
+)
+
+logger = logging.getLogger(__name__)
 # Create the FastAPI application
 app = FastAPI(title="Kitchen Service")
+
+# Initialize Prometheus metrics instrumentation
+Instrumentator().instrument(app).expose(app)
 
 # Configuration: How many cooks are available in the kitchen
 NUM_COOKS = 1
@@ -19,20 +32,27 @@ cook_semaphore = asyncio.Semaphore(NUM_COOKS)
 class SmoothieOrder(BaseModel):
     flavor: str
 
+# message on initalization
+logger.info(f"Iniatilized {__name__} with {NUM_COOKS} cook(s) available.")
+
 # Endpoint: Receives requests to prepare a smoothie
 @app.post("/prepare")
 async def prepare_smoothie(order: SmoothieOrder):
+    smoothies_ordered.labels(flavor=order.flavor).inc()
+
+    logger.info(f"Preparing smoothie", extra={"tags":{"flavor": order.flavor, "num_cooks":  str(NUM_COOKS)}})
     try:
         # Try to get a cook (wait max 2 seconds)
+        logger.debug(f"Waiting for a cook to be available")
         await asyncio.wait_for(cook_semaphore.acquire(), timeout=2.0)
     except asyncio.TimeoutError:
-	 # All cooks are busy and timeout reached -> reject the order
+        # All cooks are busy and timeout reached -> reject the order
+        logger.error(f"No cook available. {NUM_COOKS} cooks are currently busy. Consider increasing the number of cooks.")
         raise HTTPException(status_code=503, detail="All cooks are currently busy")
-
     try:
         # Simulate preparing the smoothie (takes 1.5 to 2.5 seconds)
         await asyncio.sleep(random.uniform(1.5, 2.5))
-
+        logger.info(f"Finished preparing {order.flavor} smoothie")
         return {"status": "done", "flavor": order.flavor}
     finally:
         # Release the cook so they can prepare the next smoothie
